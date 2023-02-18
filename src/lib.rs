5b694cc11124e4721cf5f29f87997e7a788ea83e
Albert Laursen <alaur20@student.aau.dk>
2023-02-18 21:31:59 +0100

    Remove Unnecessary Line in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
 #![no_std]
 
-
-use core::sync::atomic::{AtomicUsize, Ordering};
-
 use defmt_rtt as _; // global logger
 use panic_probe as _; // panic handler
 use stm32f4xx_hal as _; // memory layout
